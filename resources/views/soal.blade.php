94d21d5d5489000a79bb0bc1419124ce59ee1650
LAPTOP-EK1RD5MC\ASUS-PC <60900119033@uin-alauddin.ac.id>
2021-11-09 12:10:15 +0800

    commit soal VSC nomor 6

--- resources/views/soal.blade.php
+++ resources/views/soal.blade.php
@@ -8,5 +8,18 @@
 </head>
 <body>
     <p>ekaway</p>
+    <p>Version Control System Merupakan Suatu paket software dimana ketika kita meninisialisasi, VCS akan memonitor file kita untuk setiap perubahan dan mengizinkan kita untuk menandai perubahan tersebut pada setiap level perubahan , Sehingga kita dapat meninjau ulang tahapan yang ditandai kapan pun ketika dibutuhkan .</p>
+    <ul>
+        <li>GIT</li>
+        <li>CVS</li>
+        <li>SVN</li>
+        <li>Mercurial</li>
+        <li>monotone</li>
+        <li>BAZAAR</li>
+        <li>TFS</li>
+        <li>VSTS</li>
+        <li>perforce helix core</li>
+        <li>IBM Rational ClearCase</li>
+    </ul>
 </body>
 </html>
