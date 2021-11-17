4e95d289d70bfb36bde4767bd7cbb8204e8911f1
LAPTOP-EK1RD5MC\ASUS-PC <60900119033@uin-alauddin.ac.id>
2021-11-17 08:23:55 +0800

    commit baru lagi

--- resources/views/soal.blade.php
+++ resources/views/soal.blade.php
@@ -7,7 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-    <p>ekaway</p>
+    <p>eka wahyuni darmayanti</p>
     <p>Version Control System Merupakan Suatu paket software dimana ketika kita meninisialisasi, VCS akan memonitor file kita untuk setiap perubahan dan mengizinkan kita untuk menandai perubahan tersebut pada setiap level perubahan , Sehingga kita dapat meninjau ulang tahapan yang ditandai kapan pun ketika dibutuhkan .</p>
     <ul>
         <li>GIT</li>
